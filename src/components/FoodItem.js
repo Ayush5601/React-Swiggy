ee89f346b6b6d9351843578fdee886f47070b9e8
Ayush Agrawal <ayush123ngp@gmail.com>
2023-12-09 03:05:58 +0530

    fixed price not getting displayed for cart items

--- src/components/FoodItem.js
+++ src/components/FoodItem.js
@@ -1,12 +1,20 @@
 import { IMG_CDN_URL } from "../contants";
 
-const FoodItem = ({ name, description, imageId, price }) => {
+const FoodItem = ({ name, description, imageId, price, defaultPrice }) => {
   return (
-    <div className="w-56 p-2 m-2 shadow-lg bg-pink-50 rounded-lg">
+    <div className="w-52 p-2 m-4 h-80 shadow-lg bg-pink-50 rounded-lg cursor-pointer relative hover:bg-gray-200">
       <img className="rounded-lg" src={IMG_CDN_URL + imageId} />
-      <h2 className="font-bold text-xl">{name}</h2>
-      <h3>{description}</h3>
-      <h4>Rupees: {price / 100}</h4>
+      <h2 className="font-bold text-xl">
+        {name.substring(0, Math.min(name.length, 20))}
+      </h2>
+      <h3 className="italic my-2 overflow-y-autoo">
+        {description?.length > 70
+          ? description.substring(0, 100) + "..."
+          : description}
+      </h3>
+      <h4 className="font-bold absolute bottom-2">
+        ₹ : {(defaultPrice || price) / 100}
+      </h4>
     </div>
   );
 };
